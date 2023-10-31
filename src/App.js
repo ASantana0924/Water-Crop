105bb912ad97b0aa518a59f907ee3ef063906180
ASantana0924 <asantana1@ufl.edu>
2023-10-31 13:35:48 -0400

    Main page created with indicators added where sensor information will be displayed.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./styles.css";
 import React from "react";
-import Counter from "./Counter";
 import Name from "./Name";
 
 export default function App() {
   return (
     <div className="App">
       <Name />
-      <Counter />
     </div>
   );
 }
--- src/Name.js
+++ src/Name.js
@@ -4,7 +4,11 @@ import React from "react";
 export default function Name() {
   return (
     <div>
-      <h1> My name is Liam</h1>
+      <h1> Welcome to WaterCrop!</h1>
+      <body>
+        <p>Water Level: <span id="water_level"></span></p>
+        <p>Moisture Level: <span id="moisture_level"></span></p>
+      </body>
     </div>
   );
 }
