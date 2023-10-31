1d126b32a83c94d061c600349a3edf6929786b2e
Liam Sy <liamrs222@gmail.com>
2023-10-31 14:49:54 -0400

    receiver server data

--- src/SensorData.js
+++ src/SensorData.js
@@ -1,13 +1,24 @@
 import "./styles.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SensorData() {
-  return (
-    <div>
-      <body>
-        <p>Water Level: <span id="water_level"></span></p>
-        <p>Moisture Level: <span id="moisture_level"></span></p>
-      </body>
-    </div>
+    const [waterLevel, setWaterLevel] = useState("");
+    const [moistureLevel, setMoistureLevel] = useState("");    
+    useEffect(() => {
+    fetch("http://localhost:8000/data")
+        .then((res) => res.json())
+        .then((data) => {
+            setWaterLevel(data.waterLevel);
+            setMoistureLevel(data.moistureLevel);
+        })
+    }, []);
+    
+    return (
+        <div className="App">
+            <body>
+                <p>Water Level: {waterLevel} <span id="water_level"></span></p>
+                <p>Moisture Level: {moistureLevel} <span id="moisture_level"></span></p>
+            </body>
+        </div>
   );
 }
