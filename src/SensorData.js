f4052209fdd87180a89d1dbf8cf0b1dac592a7f9
Alejandro Santana <asantana1@ufl.edu>
2023-11-03 12:18:06 -0400

    Data page now displays different text based on sensor values

--- src/SensorData.js
+++ src/SensorData.js
@@ -18,12 +18,26 @@ export default function SensorData() {
         const interval = setInterval(refreshData, 500);
         return () => clearInterval(interval);
     }, []);
+
+    // Conditional statement to display different text based on water level
+    const waterLevelText = waterLevel === 0
+    ? "No water present"
+    : "Water present";
+
+    // Conditional statements to display different text based on moisture level
+    const moistureLevelText = moistureLevel >= 520
+    ? "Not in soil"
+    : moistureLevel >= 430
+        ? "Soil is Dry"
+        : moistureLevel >= 350
+            ? "Soil is Wet"
+            : "Submerged in Water";
     
     return (
         <div className="App">
             <body>
                 <p className="statistics">Plant Statistics:</p>
-                <p>Water Level: {waterLevel} <span id="water_level"></span></p>
+                <p>Water Level: {waterLevelText} <span id="water_level"></span></p>
                 <p>Moisture Level: {moistureLevel} <span id="moisture_level"></span></p>
             </body>
         </div>
