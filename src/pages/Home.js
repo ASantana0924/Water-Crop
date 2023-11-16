3ccde77a1a276cd71163228b61b492bac669c394
Alejandro Santana <asantana1@ufl.edu>
2023-11-15 20:23:20 -0500

    Revert "Added Home Page"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,11 @@
-import React, { useState } from "react";
 import "../styles.css";
+import React from "react";
 
 export default function Home() {
-  // Example state to demonstrate dynamic content
-  const [plantData, setPlantData] = useState({
-    name: "Sunflower",
-    description: "Plant 1: Sunflower",
-    imageUrl: `${process.env.PUBLIC_URL}/images/sunflower.jpg`,
-  });
-
-  // A function can use "setPlantData" to change the plantData
-  return (
-    <div className="App">
-      <h1 className="Header">Welcome to WaterCrop!</h1>
-      <p className="PageDescription">This page shows plants connected to your profile, along with an option to update the plant.</p>
-      <div className="ButtonContainer">
-        <button onClick={() => handleAddPlant()}>Add Plant</button>
-        <button onClick={() => handleEditPlant()}>Edit Plant</button>
-      </div>
-      <div className="CenterContainer">
-        <div className="Profiles">
-          <p className="Description">{plantData.description}</p>
-          <div id="Plant">
-            <img src={plantData.imageUrl} alt={plantData.name} />
-          </div>
-          <button onClick={() => handleStatsPlant()}>View Statistics!</button>
+    return (
+        <div>
+            <h1>Welcome to WaterCrop!</h1>
+            <p>This is the home page of your WaterCrop app.</p>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
-
-const handleAddPlant = () => {
-  // For example, you can update the state or navigate to a different page
-  console.log("Adding a new plant!");
-};
-
-const handleEditPlant = () => {
-  // For example, you can open a modal or navigate to a different page
-  console.log("Editing the current plant!");
-};
-
-const handleStatsPlant = () => {
-  // For example, you can open a modal or navigate to a different page
-  console.log("Editing the current plant!");
-};
--- src/pages/PlantProfiles.js
+++ src/pages/PlantProfiles.js
@@ -4,8 +4,8 @@ import React from "react";
 export default function PlantProfiles() {
     return (
         <div>
-            <h1>Welcome to WaterCrop!</h1>
-            <p>This is the home page of your WaterCrop app.</p>
+            <h1>Plant Profile</h1>
+            <p>More will be added here later.</p>
         </div>
     );
 }
