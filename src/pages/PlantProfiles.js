f744103859b95742ae5988e9aaa27b479b952882
ASantana0924 <asantana1@ufl.edu>
2024-02-07 13:32:43 -0500

    Added progress bar

--- src/pages/PlantProfiles.js
+++ src/pages/PlantProfiles.js
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from "react";
 import "./PlantProfiles.css";
 import { RTDBRef } from "../firebase/firebase";
 import { onValue } from "firebase/database";
+import { SemiCircleProgress } from "react-semicircle-progressbar";
+import ProgressBar from "../tools/ProgressBar";
 
 
 export default function PlantProfiles({ plant, updatePlant, history, updateHistory}) {
     // Initialize useState variables for plant profile page
     const [chartData, setChartData] = useState("Moisture");
+
+    const [moisturePercentage, setMoisturePercentage] = useState(0);
+    const [waterPercentage, setWaterPercentage] = useState(0);
+    const [temperaturePercentage, setTemperaturePercentage] = useState(0);
+    const [PHPercentage, setPHPercentage] = useState(0);
 
     useEffect(() => {
         // Listen for changes in the database
@@ -53,6 +60,11 @@ export default function PlantProfiles({ plant, updatePlant, history, updateHisto
                 moistureNum: snapshot.val().moisture,
                 waterLevel: waterLevelString
             };
+        
+            setMoisturePercentage(Number(((19000 - snapshot.val().moisture)/19000) * 100).toFixed(1));
+            setWaterPercentage(snapshot.val().water_level * 100);
+            setTemperaturePercentage((snapshot.val().temperature/100) * 100);
+            setPHPercentage(50);
 
             updateHistory(prevHistory => [data, ...prevHistory])
         });
@@ -108,9 +120,9 @@ export default function PlantProfiles({ plant, updatePlant, history, updateHisto
                 <div className="PlantText">
                     <h1>Plant {plant.id}: {plant.name} </h1>
                     <h2>Description: {plant.summary} </h2>
-                    <div className="HomeButton">
-                        <button onClick={() => returnHome()}>Home</button>
-                    </div>
+                    {/*<div className="HomeButton">
+                            <button onClick={() => returnHome()}>Home</button>
+                        </div>*/}
                 </div>
                 <div className="PlantImage">
                     <img src={plant.imageLink} alt="plant"/>
@@ -120,15 +132,20 @@ export default function PlantProfiles({ plant, updatePlant, history, updateHisto
             <div className="StatsTabs">
                 <h1>Live Stats:</h1>
                 <div class="tabs">
+                    Moisture<ProgressBar percentage={moisturePercentage} color={"#90EE90"} text=""/>
+                    Water<ProgressBar percentage={waterPercentage} color={"#90EE90"} text=""/>
+                    Temperature<ProgressBar percentage={temperaturePercentage} color={"#90EE90"} text=" F"/>
+                    PH<ProgressBar percentage={PHPercentage} color={"#90EE90"}/>
+                    {/*
                     <button class="tablinks" onClick={() => setChartData("Moisture")} autofocus>Moisture: {plant.stats.moisture}</button>
                     <button class="tablinks" onClick={() => setChartData("Water Level")} >Water Level: {plant.stats.waterLevel}</button>
-                    <button class="tablinks" onClick={() => setChartData("Temperature")}>Temp: {plant.stats.temp}</button>
+                <button class="tablinks" onClick={() => setChartData("Temperature")}>Temp: {plant.stats.temp}</button>*/}
                 </div>
-                <div class="tabs">
+                {/*<div class="tabs">
                     <button class="tablinks" onClick={() => setChartData("Nitrogen")}>Nitrogen: {plant.stats.nitrogen}</button>
                     <button class="tablinks" onClick={() => setChartData("Phosphorus")}>Phosphorus: {plant.stats.phosphorus}</button>
                     <button class="tablinks" onClick={() => setChartData("Potassium")}>Potassium: {plant.stats.potassium}</button>
-                </div>
+            </div>*/}
                 <h1>{chartData} History:</h1>
                 <div className="Table-container">
                     <table>
