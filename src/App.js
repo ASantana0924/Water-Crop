43a136516acc5b89e664a040c5b318bbf532e88f
ASantana0924 <asantana1@ufl.edu>
2024-02-08 12:35:00 -0500

    Removed navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from "react-route
 import "./styles.css";
 import React , { useState, useCallback } from "react";
 
-import Navbar from "./tools/Navbar";
 import Home from "./pages/Home";
 import PlantProfiles from "./pages/PlantProfiles";
 import Stats from "./pages/Stats";
@@ -48,15 +47,8 @@ function AppContent() {
 
   const location = useLocation();
 
-  // Define an array of paths where you want to display the Navbar
-  const showNavbarPaths = ["/home", "/plant-profiles", "/stats"];
-
-  // Check if the current path is in the array of paths where Navbar should be displayed
-  const shouldShowNavbar = showNavbarPaths.includes(location.pathname);
-
   return (
     <>
-      {shouldShowNavbar && <Navbar />}
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/" element={<Login />} />
